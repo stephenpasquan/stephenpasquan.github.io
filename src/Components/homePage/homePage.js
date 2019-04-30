7747d51882c814eb5c9a838f0b30e88a331a997e
Stephen Pasquan <stephenpasquan@gmail.com>
2019-04-29 20:53:40 -0700

    Router Completed

--- src/Components/homePage/homePage.js
+++ src/Components/homePage/homePage.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import {NavBar} from '../navbar/NavBar';
 import './homePage.css';
+
 // MAKE SURE TO CREDIT ICON8!!!!!!!!!!! (icons8.com)
 export class HomePage extends React.Component {
   render() {
+
+    const pageInfo = {
+      button: {
+        status: true,
+        buttonText: "Book Now",
+        buttonLink: "https://book.bookingcenter.com/02/?site=COLINN"
+      },
+      currPage: "homepage",
+    };
+
     return (
       <div className="body">
-        <NavBar bgImg={'GoldenGateBridge.jpg'}/>
-        <div className="flex-container">
-          <div id="c1">
-            <img src={require("../../Images/Wifi.png")} alt="wifi" />
-            <h1>Free Wifi</h1>
+        <NavBar bgImg={'GoldenGateBridge.jpg'} pageInfo={pageInfo}/>
+        <div className={"flex-container"}>
+          <div className={"column-div"}>
+            <img src={require("../../Images/Wifi.png")} alt="wifi" className={"icon-image"}/>
+            <h1 className={"home-amenities"}>Free Wifi</h1>
           </div>
-          <div id="c2">
-            <img src={require("../../Images/Tv.png")} alt="TV" />
-            <h1>Complementary Satellite TV and HBO In Every Room</h1>
+          <div className={"column-div"}>
+            <img src={require("../../Images/Tv.png")} alt="TV"  className={"icon-image"}/>
+            <h1 className={"home-amenities"}>Complementary Satellite TV and HBO In Every Room</h1>
           </div>
-          <div id="c3">
-            <img src={require("../../Images/Parking.png")} alt="Parking" />
-            <h1>Complementary Parking</h1>
+          <div className={"column-div"}>
+            <img src={require("../../Images/Parking.png")} alt="Parking" className={"icon-image"}/>
+            <h1 className={"home-amenities"}>Complementary Parking</h1>
           </div>
         </div>
+        <footer>
+          <a id={"icon8-ref"} href="https://icons8.com">Icons by Icons8</a>
+          </footer>
       </div>
     );
   }
