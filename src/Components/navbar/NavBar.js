1a743ab68bf242c17a7acd008be63cac036de489
Stephen Pasquan <stephenpasquan@gmail.com>
2019-04-30 19:20:52 -0700

    Added React Strap & Inserted into navbar

--- src/Components/navbar/NavBar.js
+++ src/Components/navbar/NavBar.js
@@ -1,39 +1,85 @@
 import React from 'react';
 import './NavBar.css';
-import {NavLink} from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
 
 export class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
   render() {
-
-    console.log(this.props)
-    const button = this.props.pageInfo.button;
-    const currPage = this.props.pageInfo.currPage;
-
     return (
-      <div className={"bg-img"} style={{backgroundImage: 'url(' + require(`../../Images/${this.props.bgImg}`) + ')'}}>
-        <div className={"nav-container"}>
-          <ul className={"topnav"}>
-            <li>
-            <NavLink exact to="/" className={"navItem"}>Home</NavLink>
-            </li>
-            <li>
-              <NavLink exact to="/contactus" className={"navItem"}>Contact Us</NavLink>
-            </li>
-            <li>
-              <NavLink exact to="/aboutus" className={"navItem"}>About Us</NavLink>
-            </li>
-            <li>
-              <NavLink exact to="/localguide" className={"navItem"}>Local Guide</NavLink>
-            </li>
-            <li className={"navItem"}>
-              <a id={"book-button"} href={button.buttonLink}>Book Now</a>
-            </li>
-          </ul>
+      <div>
+        <div className={"bgImgDesktop"} style={{backgroundImage: 'url(' + require(`../../Images/${this.props.bgImg}`) + ')'}}>
+          <div className={"desktopNavContainer"}>
+            <ul className={"desktopNavBar"}>
+              <li>
+              <NavLink href="/" className={"navItem"}>Home</NavLink>
+              </li>
+              <li>
+                <NavLink exact to="/contactus" className={"navItem"}>Contact Us</NavLink>
+              </li>
+              <li>
+                <NavLink exact to="/aboutus" className={"navItem"}>About Us</NavLink>
+              </li>
+              <li>
+                <NavLink exact to="/localguide" className={"navItem"}>Local Guide</NavLink>
+              </li>
+              <li className={"navItem"}>
+                <a id={"bookNowButton"} href={"/home"}>Book Now</a>
+              </li>
+            </ul>
+          </div>
+          <div className={"buttonContainer"}>
+          <a className={"button"} href={"/home"}>Book Now</a>
+          </div>
         </div>
-        <div className={"button-container"}>
-        <a className={"button"} href={button.buttonLink}>{button.buttonText}</a>
+        <div className={"bg-img"} style={{backgroundImage: 'url(' + require(`../../Images/${this.props.bgImg}`) + ')'}}>
+          <Navbar color="faded" light>
+            <NavbarBrand href="/" className="mr-auto"></NavbarBrand>
+            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+            <Collapse isOpen={!this.state.collapsed} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink href="/">Home</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/aboutus">About Us</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/contactus">Contact Us</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/localguide">Local Guide</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/photos">Photo Gallery</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
         </div>
       </div>
     )
