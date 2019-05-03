b8262474f874078adaddbb73da8c89197cc8264a
Stephen Pasquan <stephenpasquan@gmail.com>
2019-05-03 16:35:47 -0700

    About Us 90% Complete - Need To Finish CSS Color

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { PhotoNav } from '../PhotoNav/PhotoNav';
+import './AboutUs.css';
 
 export class AboutUs extends React.Component {
   render() {
@@ -7,12 +8,50 @@ export class AboutUs extends React.Component {
       <div>
         <PhotoNav bgImg={'GoldenGateBridge.jpg'}/>
        <div className="aboutUsContainer">
-        <h1 className="aboutUsHeader">Welcome To The Marin Lodge</h1>
-        <p className="aboutUsText">Welcome to Marin Lodge where you receive hotel comfort at motel prices, all within 12 miles of San Francisco, and just 21 miles south of Wine Country. You will find us immediately North of San Rafael Town Center and only minutes from Napa County Wine Country, Muir Woods National Park, Point Reyes National Park and Seashore, Sonoma Raceway and the Marin Civic Center. We offer all the amenities that will make your stay a comfortable one. You'll find our central location to be perfect for day trips to explore the best of what the San Francisco Bay Area offers. We have been committed to quality, service and value for more than 59 years.
-        </p>
-        <ul className="amenitiesList">Amenities
-          <li className="amenity">Cable/Satellite Television</li>
-        </ul>
+        <div className="headerContainer">
+          <h1 className="header">Location</h1>
+        </div>
+        <div className="textContainer">
+          <p className="text">With our central location, you are just minutes from wine country, San Francisco, and many local landmarks</p>
+        </div>
+
+        <div className="headerContainer">
+          <h1 className="header">Value</h1>
+        </div>
+        <div className="textContainer">
+          <p className="text">At the Marin Lodge we believe in hotel quality with motel prices. You'll find that for the price, there is no better value</p>
+        </div>
+
+        <div className="headerContainer">
+          <h1 className="header">Satisfaction</h1>
+        </div>
+        <div className="textContainer">
+          <p className="text">
+          Being a family owned company for over 60 years, we have learned that hospitality and guest satisfaction is our highest priority
+          </p>
+        </div>
+
+        <div className="headerContainer">
+          <h1 className="header">Rooms And Amenities</h1>
+        </div>
+        <div className="textContainer">
+          <p className="text">
+          We are a non-smoking hotel with several different room  layouts, so whether you are traveling with the family or for business we have a room for you
+          </p>
+        </div>
+        <div className="headerContainer">
+          <h1 className="amenitiesHeading">Amenities Include:</h1>
+        </div>
+        <div className="textContainer">
+          <ul className="amenitiesList">
+            <li className="amenity">Free Wifi</li>
+            <li className="amenity">Cable Television With HBO</li>
+            <li className="amenity">Complementary Parking</li>
+            <li className="amenity">Refrigerator, Microwave, and Wolfgang Puck Coffee In Every Room</li>
+            <li className="amenity">All Non-Smoking Rooms</li>
+            <li className="amenity">Free Starbucks Coffee and Baked Goods In The Morning</li>
+          </ul>
+        </div>
        </div>
       </div>
     )
