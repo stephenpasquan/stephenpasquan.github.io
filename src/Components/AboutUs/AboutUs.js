cb2f4b53c200aed062be33f21a98053b7bad475b
Stephen Pasquan <stephenpasquan@gmail.com>
2019-05-02 20:43:25 -0700

    Started About Us

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -6,7 +6,14 @@ export class AboutUs extends React.Component {
     return (
       <div>
         <PhotoNav bgImg={'GoldenGateBridge.jpg'}/>
-        <h1>ABOUT US</h1>
+       <div className="aboutUsContainer">
+        <h1 className="aboutUsHeader">Welcome To The Marin Lodge</h1>
+        <p className="aboutUsText">Welcome to Marin Lodge where you receive hotel comfort at motel prices, all within 12 miles of San Francisco, and just 21 miles south of Wine Country. You will find us immediately North of San Rafael Town Center and only minutes from Napa County Wine Country, Muir Woods National Park, Point Reyes National Park and Seashore, Sonoma Raceway and the Marin Civic Center. We offer all the amenities that will make your stay a comfortable one. You'll find our central location to be perfect for day trips to explore the best of what the San Francisco Bay Area offers. We have been committed to quality, service and value for more than 59 years.
+        </p>
+        <ul className="amenitiesList">Amenities
+          <li className="amenity">Cable/Satellite Television</li>
+        </ul>
+       </div>
       </div>
     )
   }
